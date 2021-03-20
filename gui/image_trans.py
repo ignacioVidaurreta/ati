042a7372a69dec1eac49201d1217563fc4d0f58b
marina <mfuster@itba.edu.ar>
2021-03-20 10:40:37 -0300

    :sparkles: ADD: Umbralization with umbral as parameter

--- gui/image_trans.py
+++ gui/image_trans.py
@@ -30,16 +30,26 @@ class ImageTransformTab(QWidget):
 
         # Buttons definitions
         self.negative = newButton("Negative", self.onNegativeClick)
-
+        self.umbralization = newButton("Umbralization", self.onUmbralizationClick)
+        
+        # Labels for umbral input
+        self.umbralLabel = QLabel("Umbral")
+        self.umbralInput = QLineEdit()
+        self.umbralInput.setText('255')
 
         # We add widgets to layout
-        self.layout.addWidget(self.negative, 1, 1)
+        self.layout.addWidget(self.negative, 1, 0)
+        self.layout.addWidget(self.umbralLabel, 2, 0)
+        self.layout.addWidget(self.umbralInput, 2, 1)
+        self.layout.addWidget(self.umbralization, 3, 0)
 
         self.setLayout(self.layout)
 
     # Convention: on[ButtonName]Click
     def onNegativeClick(self):
+        # We do not want to loose original image
         img = self.parent.image.copy()
+
         if len(self.imageShape) == 3:
             # if image has r,g,b channels, apply the transform
             # to every channel
@@ -66,7 +76,38 @@ class ImageTransformTab(QWidget):
         pixels = img.load()
         for x,y in np.ndindex(img.size):
             pixels[x,y] = 255 - pixels[x,y]
+    
+    def onUmbralizationClick(self):
+        self.umbralValue = int(self.umbralInput.text())
+        print(f"About to apply umbralization with umbral {self.umbralValue}")
 
+        # We do not want to loose original image
+        img = self.parent.image.copy()
 
-
-
+        if len(self.imageShape) == 3:
+            # if image has r,g,b channels, apply the transform
+            # to every channel
+            r,g,b = img.split()
+            self.umbralizationTransform(r)
+            self.umbralizationTransform(g)
+            self.umbralizationTransform(b)
+            # We now need to merge the modified values into an RGB image
+            img = Image.merge("RGB", (r,g,b))
+            hdisplay([self.parent.image, img], rows=1, cols=2, titles=[
+                "Original Image",
+                "Umbralization of Image"
+            ])
+        else:
+            self.umbralizationTransform(img)
+            hdisplay([self.parent.image, img], rows=1, cols=2, titles=[
+                "Original Image",
+                f"Umbralization with Umbral={self.umbralValue}"
+            ], cmap="gray")
+    
+    def umbralizationTransform(self, img):
+        pixels = img.load()
+        for x,y in np.ndindex(img.size):
+            if pixels[x,y] < self.umbralValue:
+                pixels[x,y] = 0
+            else:
+                pixels[x,y] = 255
