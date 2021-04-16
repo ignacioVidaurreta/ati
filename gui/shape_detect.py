f465befb346d7d7bd208bb5d21026a992db10e35
marina <mfuster@itba.edu.ar>
2021-04-16 08:36:46 -0300

    :sparkles: ADD: Merge of dI/dx and dI/dy

--- gui/shape_detect.py
+++ gui/shape_detect.py
@@ -14,8 +14,10 @@ from utils import (
     newButton, 
     compute_histogram,
     display_before_after, 
+    numpy_to_pil_image,
     TRANSFORMATION_FOLDER
 )
+from display import hdisplay
 from filters import Filter
 
 class ShapeDetectTab(QWidget):
@@ -48,9 +50,9 @@ class ShapeDetectTab(QWidget):
 
             r,g,b = image[0], image[1], image[2]
 
-            filter_r = PrewittFilter(r, is_dx=True)
-            filter_g = PrewittFilter(g, is_dx=True)
-            filter_b = PrewittFilter(b, is_dx=True)
+            filter_r = PrewittFilter(r, mode=Modes.DX)
+            filter_g = PrewittFilter(g, mode=Modes.DX)
+            filter_b = PrewittFilter(b, mode=Modes.DX)
 
             r_result = filter_r.apply(normalize=True)
             g_result = filter_g.apply(normalize=True)
@@ -65,11 +67,9 @@ class ShapeDetectTab(QWidget):
                 submit=False
             )
 
-            r,g,b = image[0], image[1], image[2]
-
-            filter_r = PrewittFilter(r, is_dx=False)
-            filter_g = PrewittFilter(g, is_dx=False)
-            filter_b = PrewittFilter(b, is_dx=False)
+            filter_r = PrewittFilter(r, mode=Modes.DX)
+            filter_g = PrewittFilter(g, mode=Modes.DY)
+            filter_b = PrewittFilter(b, mode=Modes.DY)
 
             r_result = filter_r.apply(normalize=True)
             g_result = filter_g.apply(normalize=True)
@@ -77,50 +77,74 @@ class ShapeDetectTab(QWidget):
 
             np_img_y = (r_result, g_result, b_result)
 
-            display_before_after(
-                self.parent, 
-                np_img_x, 
-                f'Prewitt Filter (dI/dy)',
-                submit=False
+            hdisplay(
+                [numpy_to_pil_image(np_img_x), numpy_to_pil_image(np_img_y)], 
+                rows=1, 
+                cols=2, 
+                titles=['Prewitt Filter (dI/dx)', 'Prewitt Filter (dI/dy)']
             )
+
+            filter_r = PrewittFilter(r)
+            filter_g = PrewittFilter(g)
+            filter_b = PrewittFilter(b)
+
+            r_result = filter_r.apply(normalize=True)
+            g_result = filter_g.apply(normalize=True)
+            b_result = filter_b.apply(normalize=True)
+
+            np_img = (r_result, g_result, b_result)
 
         else:
-            prewitt_filter = PrewittFilter(image, is_dx=True)
+            prewitt_filter = PrewittFilter(image, mode=Modes.DX)
             np_img_x = prewitt_filter.apply(normalize=True)
             
-            display_before_after(
-                self.parent, 
-                np_img_x, 
-                f'Prewitt Filter (dI/dx)',
-                submit=False
-            )
 
-            prewitt_filter = PrewittFilter(image, is_dx=False)
+            prewitt_filter = PrewittFilter(image, mode=Modes.DY)
             np_img_y = prewitt_filter.apply(normalize=True)
 
-            display_before_after(
-                self.parent, 
-                np_img_y, 
-                f'Prewitt Filter (dI/dy)',
-                submit=False
+            hdisplay(
+                [numpy_to_pil_image(np_img_x), numpy_to_pil_image(np_img_y)], 
+                rows=1, 
+                cols=2, 
+                titles=['Prewitt Filter (dI/dx)', 'Prewitt Filter (dI/dy)'],
+                cmap="gray"
             )
+            
+            prewitt_filter = PrewittFilter(image)
+            np_img = prewitt_filter.apply(normalize=True)
 
+        display_before_after(
+            self.parent, 
+            np_img, 
+            f'Prewitt Filter',
+            submit=False
+        )
+
+
+class Modes:
+        DX = 0
+        DY = 1
+        FULL = 2
 
 class PrewittFilter(Filter):
-    """
-    TODO: implement synthesis to join dx and dy.
-    """
-    def __init__(self, image, is_dx):
+       
+    def __init__(self, image, mode=Modes.FULL):
         super().__init__(image, L=3)
 
-        self.weighted_mask = [4,2,1]
-        self.is_dx = is_dx
+        self.mode = mode
 
     def compute(self, pixels, x, y):
-        value = 0
+        x_value = 0
+        y_value = 0
         for x_index in range(-1 * self.mid, self.mid + 1):
             for y_index in range(-1 * self.mid, self.mid + 1):
-                mult = y_index if self.is_dx else x_index
-                value += pixels[x + x_index, y + y_index] * mult
+                #mult = y_index if self.is_dx else x_index
+                x_value += pixels[x + x_index, y + y_index] * y_index
+                y_value += pixels[x + x_index, y + y_index] * x_index
 
-        return value
+        if self.mode == Modes.DX:
+            return x_value
+        elif self.mode == Modes.DY:
+            return y_value
+        else:
+            return math.sqrt(x_value ** 2 + y_value ** 2)
