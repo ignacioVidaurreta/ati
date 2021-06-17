a364dca5d714decb89f6accd6b7f296e56e7f5d8
Sofía Picasso <spicasso@itba.edu.ar>
2021-06-17 16:47:11 -0300

    Added max and avg for Harris

--- gui/advanced_borders.py
+++ gui/advanced_borders.py
@@ -37,10 +37,19 @@ class AdvancedBordersTab(QWidget):
         # self.t1_label, self.t1_input = QLabel("t1"), QLineEdit()
         # self.t1_input.setText("70")
         self.harris_btn = newButton("Apply", self.onHarrisClick)
+        self.max_label, self.max_input = QLabel("Max:"), QLineEdit()
+        self.max_input.setText("0.001")
+
+        self.max_label2, self.max_input2 = QLabel("Avg:"), QLineEdit()
+        self.max_input2.setText("100000000")
 
         # We add widgets to layout
         self.layout.addWidget(self.harris, 0, 0)
-        self.layout.addWidget(self.harris_btn, 0, 1)
+        self.layout.addWidget(self.harris_btn, 0, 5)
+        self.layout.addWidget(self.max_label, 0, 1)
+        self.layout.addWidget(self.max_input, 0, 2)
+        self.layout.addWidget(self.max_label2, 0, 3)
+        self.layout.addWidget(self.max_input2, 0, 4)
         self.setLayout(self.layout)
 
 
@@ -63,15 +72,37 @@ class AdvancedBordersTab(QWidget):
         k = 0.04
         R = (i_xx * i_yy - np.square(i_xy)) - k * np.square((i_xx + i_yy))
 
-        max_val = 0.1
+        Ravg = (R-np.min(R))/np.ptp(R)
+        Ravg = Ravg.mean()
+        max_val = float(self.max_input.text())
+        max_val2 = float(self.max_input2.text())
+
+
         # Esta umbralización hay un problema, habría que superponerla con la imágen para que
         # muestre las esquinas. Según Juli con un valor > 0 vale para que sea esquina. Pero 0 no es
         # un buen valor porque hace las esquinas mnuy gruesas (ver la grabación de la última clase).
         # Podríamos poner el umbral como parámetro para probar
-        umbralized = np.array([list(map(lambda x: 255 if x > max_val else 0, col)) for col in R])
 
-        display_before_after(
-            self.parent,
-            umbralized,
-            f"Harris Method - Umbral {max_val}"
-        )
+        #umbralized = np.array([list(map(lambda x: 255 if x > max_val*np.max(R) else 0, col)) for col in R])
+        fig, ax = plt.subplots(ncols = 2)
+
+        X = []
+        Y = []
+
+        X2 = []
+        Y2 = []
+
+        for x,y in np.ndindex(self.image.shape):
+            if(R[x][y] > max_val2*Ravg):
+                X.append(y)
+                Y.append(x)
+            if(R[x][y] > max_val*np.max(R)):
+                X2.append(y)
+                Y2.append(x)
+
+        ax[0].imshow(self.parent.changes[-1], cmap='gray')
+        ax[0].scatter(X2,Y2,1,c='green')
+        ax[1].imshow(self.parent.changes[-1], cmap='gray')
+        ax[1].scatter(X,Y,1,c='red')
+        fig.show()
+
